8a2b76d5c2c424230e99b7881407d3fa4723ae25
Chris <chrisantymargarita@gmail.com>
2024-06-11 02:42:59 +0700

    update demo playwright

--- tests/6. verify_profile_without_login.test.js
+++ tests/6. verify_profile_without_login.test.js
@@ -6,10 +6,7 @@ test('Navigate to profile without login on demoqa.com/books', async ({ page }) =
 
   // Click on the profile link to navigate to the profile page
   await page.click('text=Profile');
-
-  // Wait for the page to load
-  await page.waitForLoadState('networkidle');
-
+  
   // Wait for the error message element to appear
-  const errorMessageElement = await page.waitForSelector('text=Currently you are not logged', { timeout: 5000 });
+  const errorMessageElement = await page.waitForSelector('text=Currently you are not logged');
 });
